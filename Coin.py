e131592bc22b31d5658d732aab6351a8ec968e53
robuntu <robuntu@stro.ycdsb.ca>
2019-03-22 09:49:43 -0400

    test commit

--- Coin.py
+++ Coin.py
@@ -1,7 +1,32 @@
+
+import random
+
 # Define your Coin class here
+class Coin():
+  def __init__(self):
+    self.face = random.choice(["heads", "tails"])
+
+  def get_face(self):
+    return self.face
+
+  def flip(self):
+    return random.choice(["heads", "tails"])
 
 
 if __name__ == '__main__':
-  # put your code that utilizes your Coin class here
-  pass
+  coin1 = Coin()
+  coin1_heads = 0
+  coin1_tails = 0
+
+  for i in range(1000):
+    coin1.flip()
+    if coin1.get_face() == "heads":
+      coin1_heads += 1
+    else:
+      coin1_tails += 1
+  print(coin1_heads)
+  print(coin1_tails)
+
+
+
   
